863bdc7186962c2d68049a35d83e534d1d39e5b9
Lucanamon <holy_agnus@hotmail.com>
2026-04-03 14:57:03 +0700

    create and sorting house

---
+++ apps/api/controllers/controllers.ts
@@ -0,0 +1,15 @@
+﻿// TestController.cs
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { message = "Hello from Clinicrest API 🚀" });
+    }
+}
